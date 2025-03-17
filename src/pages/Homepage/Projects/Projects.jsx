f3f64d2edb70c4e5a05b133cdeb5ee57990a46f8
Romain LEUNIS <romain.leunis@mousquetaires.com>
2025-03-17 16:51:17 +0100

    projects

--- src/pages/Homepage/Projects/Projects.jsx
+++ src/pages/Homepage/Projects/Projects.jsx
@@ -3,12 +3,18 @@ import Project from '../../../components/Project/Project';
 
 import ImgEventsw from '../../../assets/eventsw.png';
 import ImgPanepinto from '../../../assets/panepinto.png';
+import ImgMavie from '../../../assets/mavie.png';
+import ImgTrombi from '../../../assets/trombi.png';
 
 const Projects = () => {
     return(
         <div className="Projects">
             <span className='title'>My work</span>
             <div className="container-project">
+                <Project img={ImgEventsw} nom="EventSW" dark={true} />
+                <Project img={ImgPanepinto} nom="Studio Panepinto" dark={false} />
+                <Project img={ImgMavie} nom="Ma Vie Mousquetaires" dark={true} />              
+                <Project img={ImgTrombi} nom="Trombinoscope Mousquetaires" dark={false} />
                 <Project img={ImgEventsw} nom="EventSW" dark={true} />
                 <Project img={ImgPanepinto} nom="Studio Panepinto" dark={false} />
             </div>
